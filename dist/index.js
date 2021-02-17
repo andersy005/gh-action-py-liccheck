f5cc379ab9fffd7e91f16dc3d0fd184627a034c8
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-02-16 23:20:06 -0700

    Set `continue-on-error` in CI

--- dist/index.js
+++ dist/index.js
@@ -132,7 +132,7 @@ function run() {
             const report = yield readFileAndApplyStyle(inputs.reportingTxtFile, style.bold.open, style.bold.close);
             core.info(report);
             if (errors.status === true) {
-                core.setFailed(`${errors.message}. Found incompatible and/or unknown licenses.${style.bold.open}For more information, check the 'Running the license checker...' and 'License checker report' sections..${style.bold.close}`);
+                core.setFailed(`${errors.message}. Found incompatible and/or unknown licenses. ${style.bold.open}For more information, check the 'Running the license checker...' and 'License checker report' sections..${style.bold.close}`);
             }
         }
         catch (error) {
